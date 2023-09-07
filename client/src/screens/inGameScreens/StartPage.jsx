422de5b739d7eee0421e090884a7523110d3f70c
Florian59c <florian.cagnon@live.fr>
2023-09-07 21:09:49 +0200

    ajout du wallpaper et du formulaire avec fonctionalité + ajout du vrai mdp dans le script resetGames

--- client/src/screens/inGameScreens/StartPage.jsx
+++ client/src/screens/inGameScreens/StartPage.jsx
@@ -1,17 +1,35 @@
 // La page d'entrée ou on tape le mdp utilisateur
 import { useFindGameCodeQuery } from '../../gql/generated/schema';
 import './css/StartPage.css';
+import Wallpaper from '../../img/Wallpaper1.png'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function StartPage() {
+
+  const [psw, setPsw] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const pageCode = useFindGameCodeQuery({ variables: { data: "StartPage" } });
   const gameCode = pageCode?.data?.findGameCode;
 
-  console.log("le code de la page est dans la constante gameCode et vaux : " + gameCode);
+  // console.log("le code de la page est dans la constante gameCode et vaux : " + gameCode);
 
   return (
-    <div>
-      <p>StartPage</p>
+    <div className='start-page' style={{ backgroundImage: `url(${Wallpaper})` }}>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if (psw === gameCode) {
+          navigate("/desktop");
+        } else {
+          setError('Mot de passe incorrect');
+        }
+      }}>
+        <input type="password" id='psw' value={psw} onChange={(e) => setPsw(e.target.value)} />
+        <button type="submit" className='start-page-button'>V</button>
+      </form>
+      <p>{error}</p>
     </div>
   );
 }
