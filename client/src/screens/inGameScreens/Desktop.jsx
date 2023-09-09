644a48e2a91ceef8ea95f95e36e8dbaf29680a95
Florian59c <florian.cagnon@live.fr>
2023-09-09 14:48:09 +0200

    background + barre de démarrage

--- client/src/screens/inGameScreens/Desktop.jsx
+++ client/src/screens/inGameScreens/Desktop.jsx
@@ -1,10 +1,13 @@
 // La page du Bureau
 import './css/Desktop.css';
+import Wallpaper from '../../img/Windows10_Wallpaper.jpg';
 
 function Desktop() {
   return (
-    <div>
-      <p>Desktop</p>
+    <div className='desktop' style={{ backgroundImage: `url(${Wallpaper})` }}>
+      <div className='footer'>
+
+      </div>
     </div>
   );
 }
